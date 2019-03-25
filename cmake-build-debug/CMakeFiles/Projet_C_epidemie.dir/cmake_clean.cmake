a97233cde088f1d6cdbc4f44ad1801d530c70ff9
Batoch <baptiste.massardier@gmail.com>
2019-03-25 15:19:43 +0100

    Clion

---
+++ cmake-build-debug/CMakeFiles/Projet_C_epidemie.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_C_epidemie.dir/main.c.obj"
+  "CMakeFiles/Projet_C_epidemie.dir/zombie.c.obj"
+  "Projet_C_epidemie.pdb"
+  "Projet_C_epidemie.exe"
+  "Projet_C_epidemie.exe.manifest"
+  "libProjet_C_epidemie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_C_epidemie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
