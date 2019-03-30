54be4cd2c33c550dcc1a03d9453360d5c7332572
YAYA <makhlouf.yassine@hotmail.fr>
2019-03-30 19:32:52 +0100

    Clion2

--- main.c
+++ main.c
@@ -2,15 +2,32 @@
 #include <stdlib.h>
 #include "zombie.h"
 #include "CreerGraph.h"
+#include "graphe.h"
 
 int main()
 {
     printf("Hello world123!\n");
-    //enum eType {sain, immunise, malade, mort};
+//cvbknfcb
+    int taille; taille = 10;
+    creerGraph(taille);
 
-    creerGraph(10);
+    Graphe populationMatrice;
+    creerGraph(taille);
+    lireGraphe(&populationMatrice, "graphe.txt");
+
+    // Toute la population est initialemnt saine
+    // On infecte une personne au hasard
+    printf("//******** INFECTIONATOR V2.0 ********\\\\\n\n");
+    printf("parametres d'infection:\nchances d'infection: ");
+    float plambda; float pbeta; float pgamma;
+    scanf("%d",&plambda);
+    printf("\ndangerosite de l'infection: ");
+    scanf("%d",&pbeta);
+    printf("\nprobabilite d'immunisation: ");
+    scanf("%d",&pgamma);
+    //void infection(plambda, pbeta, pgamma);
+
+
 
     return 0;
 }
-
-
