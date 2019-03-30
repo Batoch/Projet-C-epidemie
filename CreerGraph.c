aa8b438f12e086f2969b9ac9ed676c2e88ff8f17
Batoch <baptiste.massardier@gmail.com>
2019-03-30 18:31:25 +0100

    creer graph

--- CreerGraph.c
+++ CreerGraph.c
@@ -9,6 +9,24 @@ void creerGraph(int taille){
     FILE *fp;
     fp = fopen("graph.txt", "w");
     fputs(taille*taille, fp);
+    for (int i = 1; i <= taille; ++i) {                                //i represente la i eme personne du graph
+        int voisin[i-taille, i+1, i+taille, i-1];                      //indice du voisin: haut, droite, bas, gauche
 
+        if(i<taille){
+            voisin[0]=0;                                               //Si la ieme personne fait parti de la premiere ligne, alors elle n'a pas de voisin en haut
+        }
+        if(i>taille*taille-taille){                                     //Si la ieme personne est sur la derniere ligne
+            voisin[2]=0;
+        }
+        if(taille%i==0){                                                //Si sur la colonne de droite
+            voisin[1]=0;
+        }
+        if(taille%(i+1)==0){                                            //Si sur la colonne de gauche
+            voisin[3]=0;
+        }
+        fputs("\n", fp);
+        fputs(voisin, fp);
+    }
+    return 0;
 
 }
