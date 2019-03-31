892aa1fc848593cf853ce6a3fae10d407a30b6dc
Batoch <baptiste.massardier@gmail.com>
2019-03-31 16:38:35 +0200

    update affichage

--- affichage.c
+++ affichage.c
@@ -30,3 +30,38 @@ void afficherMatrice(enum eType* listeEtats, int taille){
     }
     printf("\n");
 }
+
+void statisaiques(enum eType* listeEtats, int taille, float stats[]){
+    int malades = 0, morts = 0, sains = 0, immunises = 0;
+    int sujets = taille*taille;
+
+    FILE *fp;
+    fp = fopen("statistiques.txt", "w");
+
+    for(int i=0; i< taille*taille; i++){
+        switch(listeEtats[i]){
+            case sain:
+                sains++;
+                break;
+            case malade:
+                malades++;
+                break;
+            case immunise:
+                immunises++;
+                break;
+            case mort:
+                morts++;
+                break;
+        }
+    }
+
+    fprintf(fp, "Nombre de sujets: %d\n", sujets);
+    fprintf(fp, "Nombre de morts: %d (%d%)\n", morts, morts*100/sujets);
+    fprintf(fp, "Nombre de malades: %d (%d%)\n", malades, malades*100/sujets);
+    fprintf(fp, "Nombre d immunises: %d (%d%)\n", immunises, immunises*100/sujets);
+    fprintf(fp, "Nombre de sains: %d (%d%)\n", sains, sains*100/sujets);
+
+
+
+    fclose(fp);
+}
